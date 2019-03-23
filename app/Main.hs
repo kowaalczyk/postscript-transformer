522ac060344cdfb207c6de8eca15c9f3764e546e
Krzysztzof Kowalczyk <k.kowaalczyk@gmail.com>
2019-03-23 23:44:43 +0100

    Creating pictures without error handling

--- app/Main.hs
+++ app/Main.hs
@@ -8,8 +8,8 @@ module Main where
 
     data ParserState = ParserState {
         stack :: [R],
-        currPoint :: Maybe Point,
-        basePoint :: Maybe Point,
+        currPoint :: Maybe R2,
+        basePoint :: Maybe R2,
         currPicture :: Picture
     }
     initState = ParserState [] Nothing Nothing (Picture [])
@@ -24,16 +24,29 @@ module Main where
         | word == "sub" = applyOp (-) state
         | word == "mul" = applyOp (*) state
         | word == "div" = applyOp (/) state
+        | word == "moveto" = moveTo state
+        | word == "lineto" = lineTo state
+        | word == "closepath" = closePath state
         | otherwise = push (readR word) state where
             applyOp :: (R->R->R) -> ParserState -> ParserState
             applyOp op (ParserState (a:b:rest) cp bp pic) = ParserState ((op b a):rest) cp bp pic
             push :: R -> ParserState -> ParserState
             push n (ParserState stack cp bp pic) = ParserState (n:stack) cp bp pic
+            moveTo :: ParserState -> ParserState
+            moveTo (ParserState (a:b:rest) cp bp pic) = ParserState rest (Just (b,a)) (Just (b,a)) pic
+            moveTo state = state  -- TODO: Error handling
+            lineTo :: ParserState -> ParserState
+            lineTo (ParserState (a:b:rest) (Just cp) bp pic) = ParserState rest (Just (b,a)) bp (pic & line cp (b,a))
+            lineTo state = state  -- TODO: Error handling
+            closePath :: ParserState -> ParserState
+            closePath (ParserState stack (Just cp) (Just bp) pic) = ParserState stack (Just bp) (Just bp) (pic & line cp bp)
+            closePath state = state  -- by reqirement, closePath defaults to identity instead of error
+            -- TODO: read parse error
 
-    parseInput :: [String] -> State ParserState [R]
+    parseInput :: [String] -> State ParserState Picture
     parseInput [] = do
         (ParserState stack cp bp pic) <- get
-        return stack
+        return pic
     parseInput (word:words) = do
         currentState <- get
         let nextState = parseWord word currentState
@@ -43,8 +56,8 @@ module Main where
     main :: IO()
     main = do
         args <- getArgs
-        let n = read $ head args :: Integer
+        let n = read $ head args :: Int
         input <- getContents
         putStrLn "300 400 translate"
-        print $ evalState (parseInput . words $ input) initState
+        print $ renderScaled n $ evalState (parseInput . words $ input) initState
         putStrLn "stroke showpage"
