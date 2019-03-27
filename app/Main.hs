950e750b8b472a8c6d8b85b426a46bd6d41de52e
Krzysztof Kowalczyk <k.kowaalczyk@gmail.com>
2019-03-28 00:20:55 +0100

    Add exception handling, refactor imports, add test script

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,11 @@
 module Main where
-    import System.Environment
-    import System.IO
+    import System.Environment (getArgs)
+    import System.IO (putStrLn, getContents)
+    import Control.Monad.State (StateT, get, put, evalStateT)
+    import Control.Monad.Except (Except, throwError, runExcept)
+    import Text.Read (readMaybe)
     import Data.List
     import Data.Maybe
-    import Control.Monad.State
     import Mon
     import Lib
 
@@ -20,81 +22,93 @@ module Main where
     stackSize :: ParserState -> Int
     stackSize state = length . stack $ state
 
-    readR :: String -> R
-    readR str = toRational n where
-        n = read str :: Integer
-
-    applyOp :: (R->R->R) -> ParserState -> ParserState
+    applyOp :: (R->R->R) -> ParserState -> Except String ParserState
     applyOp op state = case stack state of
-        (a:b:rest) -> state { stack=((op b a):rest) }
-        otherwise -> state -- TODO: Error handling
+        (a:b:rest) -> return state { stack=((op b a):rest) }
+        otherwise -> throwError "StackTooSmall"
 
-    applyPush :: R -> ParserState -> ParserState
-    applyPush n state = state { stack=(n:prevStack) } where
-        prevStack = stack state
+    applyDiv :: ParserState -> Except String ParserState
+    applyDiv state = case stack state of
+        (a:0:rest) -> throwError "DivideByZero"
+        (a:b:rest) -> return state { stack=((b / a):rest) }
+        otherwise -> throwError "StackTooSmall"
 
-    applyMoveTo :: ParserState -> ParserState
+    applyPush :: String -> ParserState -> Except String ParserState
+    applyPush word state = case (readMaybe word :: Maybe Integer) of
+        (Just n) -> return state { stack=((toRational n):prevStack) } where
+            prevStack = stack state
+        Nothing -> throwError "ParsingError"
+
+    applyMoveTo :: ParserState -> Except String ParserState
     applyMoveTo state = case stack state of
-        (a:b:rest) -> state { stack=rest, cpt=newBase, clen=(Just 0), bpt=newBase } where -- TODO: Error handling
+        (a:b:rest) -> return state { stack=rest, cpt=newBase, clen=(Just 0), bpt=newBase } where
             newBase = Just $ point (b,a)
-        otherwise -> state
+        otherwise -> throwError "StackTooSmall"
 
-    applyLineTo :: ParserState -> ParserState
-    applyLineTo state = case stack state of
-        (a:b:rest) -> state { stack=rest, cpt=(Just newPoint), clen=(Just newLen), cpic=newPic } where
+    applyLineTo :: ParserState -> Except String ParserState
+    applyLineTo state = case (clen state, stack state) of
+        (Nothing,_) -> throwError "NoStartingPoint"
+        (_,(a:b:rest)) -> return state { stack=rest, cpt=(Just newPoint), clen=(Just newLen), cpic=newPic } where
             newLen = oldLen + 1
             newPoint = trpoint (ctr state) (point (b,a))
             newPic = (cpic state) & line (xy oldPoint) (xy newPoint)
-            Just oldLen = clen state -- TODO: Error handling
-            (Just oldPoint) = cpt state -- TODO: Error handling
-        otherwise -> state -- TODO: Error handling
+            Just oldLen = clen state
+            (Just oldPoint) = cpt state  -- clen is not Nothing ==> cpt is not Nothing
+        otherwise -> throwError "StackTooSmall"
 
-    applyClosePath :: ParserState -> ParserState
+    applyClosePath :: ParserState -> Except String ParserState
     applyClosePath state = case clen state <= Just 1 of
-        True -> state  -- unlike lineTo, this is not an error by design
-        False -> state { cpt=(Just newPoint), clen=(Just newLen), cpic=newPic } where
+        True -> return state  -- unlike lineTo, this is not an error by design
+        False -> return state { cpt=(Just newPoint), clen=(Just newLen), cpic=newPic } where
             newLen = oldLen + 1
             newPic = (cpic state) & line (xy oldPoint) (xy newPoint)
             (Just newPoint) = bpt state -- clen >= 2 ==> bpt is not Nothing
             (Just oldPoint) = cpt state -- clen >= 2 ==> cpt is not Nothing
             Just oldLen = clen state -- exists, and >= 2 by case definition
 
-    applyRotate :: ParserState -> ParserState
+    applyRotate :: ParserState -> Except String ParserState
     applyRotate state = case stack state of
-        (a:rest) -> state { stack=rest, ctr=newTransform } where
+        (a:rest) -> return state { stack=rest, ctr=newTransform } where
             newTransform = rotate a >< ctr state
-        otherwise -> state -- TODO: Error handling
+        otherwise -> throwError "StackTooSmall"
 
-    applyTranslate :: ParserState -> ParserState
+    applyTranslate :: ParserState -> Except String ParserState
     applyTranslate state = case stack state of
-        (a:b:rest) -> state { stack=rest, ctr=newTransform } where
+        (a:b:rest) -> return state { stack=rest, ctr=newTransform } where
             newTransform = translate (vec (b,a)) >< ctr state
-        otherwise -> state -- TODO: Error handling
+        otherwise -> throwError "StackTooSmall"
 
+    parseWord :: String -> ParserState -> Except String ParserState
     parseWord word state = case word of
         "add" -> applyOp (+) state
         "sub" -> applyOp (-) state
         "mul" -> applyOp (*) state
-        "div" -> applyOp (/) state  -- TODO: Div error handling
+        "div" -> applyDiv state
         "moveto" -> applyMoveTo state
         "lineto" -> applyLineTo state
         "closepath" -> applyClosePath state
         "rotate" -> applyRotate state
         "translate" -> applyTranslate state
-        otherwise -> applyPush (readR word) state  -- TODO: read parse error
+        otherwise -> applyPush word state
 
-    parseInput :: [String] -> State ParserState Picture
+    parseInput :: [String] -> StateT ParserState (Except String) Picture
     parseInput [] = do
         state <- get
         return $ cpic state
     parseInput (word:words) = do
         state <- get
-        let nextState = parseWord word state
-        put nextState
-        parseInput words
+        let result = runExcept $ parseWord word state
+        case result of
+            (Right nextState) -> do
+                put nextState
+                parseInput words
+            (Left e) -> throwError e
 
-    showRendering :: IntRendering -> String
-    showRendering rendering = unlines $ map showLine rendering where
+    postprocess :: Int -> Either String Picture -> String
+    postprocess _ (Left e) = e
+    postprocess picScale (Right pic) = showRendering $ renderScaled picScale pic where
+        showRendering :: IntRendering -> String
+        showRendering rendering = unlines $ map showLine rendering
         showLine :: IntLine -> String
         showLine ((x1,y1),(x2,y2)) = foldl (++) "" [show x1," ",show y1," moveto ",show x2," ",show y2," lineto"]
 
@@ -104,5 +118,5 @@ module Main where
         let n = read $ head args :: Int
         input <- getContents
         putStrLn "300 400 translate"
-        putStrLn $ showRendering $ renderScaled n $ evalState (parseInput . words $ input) initState
+        putStrLn $ postprocess n (runExcept (evalStateT (parseInput . words $ input) initState))
         putStrLn "stroke showpage"
---
+++ test.ps1
@@ -0,0 +1,17 @@
+$testFolder = ("examples")
+$scaleParam = 1
+
+$inputData = Get-ChildItem $testFolder -Filter good*.in | Sort-Object
+$expected = Get-ChildItem $testFolder -Filter good*.ps | Sort-Object
+# $inputData = Get-ChildItem $testFolder -Filter *d.in | Sort-Object
+# $expected = Get-ChildItem $testFolder -Filter *d.ps | Sort-Object
+
+For ($I=0; $I -lt $inputData.count; $I++) {
+    $in = Join-Path $testFolder ($inputData[$I])
+    $out = Join-Path $testFolder (($expected[$I]).Name.Replace("ps", "out"))
+    $exp = Join-Path $testFolder ($expected[$I])
+    Write-Output $exp
+    Write-Output $out
+    Get-Content $in | stack run $scaleParam > $out
+    Compare-Object (Get-Content $exp) (Get-Content $out) | Write-Output
+}
